4c1e34bf3b1d035dfc65bbc505f58ee7c84f4d32
thomaspsl <thomas.peissel.tp@gmail.com>
2025-12-12 18:18:05 +0100

    fix: provider

--- lib/main.dart
+++ lib/main.dart
@@ -29,16 +29,11 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider<UserProvider>(
-            create: (context) => UserProvider()),
-        ChangeNotifierProvider<ThemeProvider>(
-            create: (context) => ThemeProvider()),
-        ChangeNotifierProvider<SessionProvider>(
-            create: (context) => SessionProvider()),
-        ChangeNotifierProvider<ExerciseProvider>(
-            create: (context) => ExerciseProvider()),
-        ChangeNotifierProvider<DateProvider>(
-            create: (context) => DateProvider()),
+        ChangeNotifierProvider<UserProvider>(create: (context) => UserProvider()),
+        ChangeNotifierProvider<ThemeProvider>(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider<SessionProvider>(create: (context) => SessionProvider()),
+        ChangeNotifierProvider<ExerciseProvider>(create: (context) => ExerciseProvider()),
+        ChangeNotifierProvider<DateProvider>(create: (context) => DateProvider()),
       ],
       child: App(),
     ),
